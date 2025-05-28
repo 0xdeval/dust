b8cea4fced9a9a6cab2cb930d07b03decfa9a075
Mike <26190360+0xdeval@users.noreply.github.com>
2025-05-28 20:28:07 +0400

    Safe sdk support (#6)
    * upd og image and metadata
    * fixed lint
    * Safe sdk support

--- src/configs/wagmi.ts
+++ src/configs/wagmi.ts
@@ -1,12 +1,25 @@
-import { cookieStorage, createStorage } from "@wagmi/core";
+import { cookieStorage, type CreateConnectorFn, createStorage } from "@wagmi/core";
 import { WagmiAdapter } from "@reown/appkit-adapter-wagmi";
 import type { AppKitNetwork } from "@reown/appkit/networks";
-
+import { safe } from "wagmi/connectors";
 import { wagmiNetworks } from "@/configs/networks";
+
+interface SafeConnectorOptions {
+  allowedDomains: Array<RegExp>;
+  debug: boolean;
+}
 
 export const projectId = process.env.NEXT_PUBLIC_PROJECT_ID;
 
 if (!projectId) throw new Error("NEXT_PUBLIC_PROJECT_ID is not defined");
+
+const connectors: Array<CreateConnectorFn> = [];
+connectors.push(
+  safe({
+    allowedDomains: [/^.*$/],
+    debug: false,
+  } as SafeConnectorOptions)
+);
 
 export const wagmiAdapter = new WagmiAdapter({
   storage: createStorage({
@@ -15,6 +28,10 @@ export const wagmiAdapter = new WagmiAdapter({
   ssr: true,
   projectId,
   networks: wagmiNetworks as Array<AppKitNetwork>,
+  connectors,
+  batch: {
+    multicall: true,
+  },
 });
 
 export const config = wagmiAdapter.wagmiConfig;
